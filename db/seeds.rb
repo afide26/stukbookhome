923f0c438bca70241aade833a2735522f6668641
Alan Fidelino <afidedev26@gmail.com>
2015-08-11 19:26:44 +1000

    Added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "afide27", email: "afide27@example.com", password:"fidelino27", password_confirmation:"fidelino27")
+User.create(username: "afide28", email: "afide28@example.com", password:"fidelino28", password_confirmation:"fidelino28")
+User.create(username: "afide29", email: "afide29@example.com", password:"fidelino29", password_confirmation:"fidelino29")
+User.create(username: "afide30", email: "afide30@example.com", password:"fidelino30", password_confirmation:"fidelino30")
+User.create(username: "afide31", email: "afide31@example.com", password:"fidelino31", password_confirmation:"fidelino31")
+
+p "Test users created"

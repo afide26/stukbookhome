4e18deeda2bf7b7b7576bfbeb352deb815205191
Alan Fidelino <afidedev26@gmail.com>
2015-08-12 19:48:02 +1000

    Friendship labels added

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,17 @@ class User < ActiveRecord::Base
       if friendship.first.state == "active"
         return "friends"
       else
-      if friendship.first.user == self
-        return "pending"
-      else
-        return "requested"
+        if friendship.first.user == self
+          return "pending"
+        else
+          return "requested"
+        end
       end
-    end
-   end
+     end
   end
+
+  def friendship_relation(user_2)
+     Friendship.where(user_id:[self.id, user_2.id], friend_id: [self.id, user_2.id]).first
+  end
+
 end
